1b5e2c36c624f8653fe5f7d3e7622a132ed4178f
binpp <teamgf2002@gmail.com>
2024-04-10 17:47:21 +0700

    orifin

--- backend/src/main/java/org/example/shop/controllers/UploadFileController.java
+++ backend/src/main/java/org/example/shop/controllers/UploadFileController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,7 +28,7 @@ public class UploadFileController {
     @Value("${resources.images.directory}")
     private String imagesDirectory;
 
-
+    @CrossOrigin(origins = "*")
     @PostMapping("product/upload")
     public ResponseEntity<Response<String>> handleFileUpload(@RequestParam("file") MultipartFile file, @RequestParam("productId") String productId) {
 
