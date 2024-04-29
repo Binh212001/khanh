780d15989f71e4a34745913fddcd337709bec7a9
Binh1 <teamgf2002@gmail.com>
2024-04-29 21:05:34 +0700

    router

--- client/src/router/routes.jsx
+++ client/src/router/routes.jsx
@@ -4,10 +4,6 @@ import UpdateUser from "../pages/auth/UpdateUser.jsx";
 import Cart from "../pages/cart/components/Cart";
 import MyBill from "../pages/cart/components/MyBill.jsx";
 import Category from "../pages/category/Category.jsx";
-import Ao from "../pages/category/ao/Ao";
-import Quan from "../pages/category/quan/Quan";
-import Shose from "../pages/category/shose/Shose.jsx";
-import Vay from "../pages/category/vay/Vay";
 import Dashboard from "../pages/home/components/Dashboard";
 import Bill from "../pages/manage/Bill.jsx";
 import Home from "../pages/manage/Home";
@@ -38,36 +34,19 @@ const routes = [
     element: <Category/>,
     layout: "layout",
   },
-  {
-    path: routeConfig.ao,
-    element: <Ao />,
-    layout: "layout",
-  },
-  {
-    path: routeConfig.shose,
-    element: <Shose />,
-    layout: "layout",
-  },
+
   {
     path: routeConfig.updateUser,
     element: <UpdateUser />,
     layout: "layout",
   },
-  {
-    path: routeConfig.quan,
-    element: <Quan />,
-    layout: "layout",
-  },
+
   {
     path: routeConfig.myBill,
     element: <MyBill />,
     layout: "layout",
   },
-  {
-    path: routeConfig.vay,
-    element: <Vay />,
-    layout: "layout",
-  },
+
   {
     path: routeConfig.search,
     element: <Search />,
